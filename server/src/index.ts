026730eaeb24d179a5aae16d7e05107bc5e3ba53
Esra Bildik <sesrabildik@gmail.com>
2026-01-22 01:13:14 +0300

    Proje başlatıldı: Backend, TypeScript ve Express kurulumu tamam.

---
+++ server/src/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors' ;
+import dotenv from 'dotenv';
+
+// ayarları yüklüyoruz (.env dosyasını okur)
+dotenv.config();
+
+// uygulamayı oluşturuyoruz
+const app = express();
+
+app.use(cors()); // Güvenlik : Herkese (web/mobil) giriş izni verir. (Test için böyle, production'da değiştiricez)
+app.use(express.json()); // Tercüman: Gelen verileri JSON formatına çevirir.
+
+const PORT = process.env.PORT || 3000;
+ 
+// Endpointler
+// Ana sayfa rotası
+app.get('/', (req, res) => {
+  // req(request): gelen istek
+  // res(response): bizim vereceğimiz cevap
+  res.send('FocusLobby sunucusu yayında');
+});
+
+// Test rotası : 
+app.get('/test', (req,res) => {
+  res.json({
+    mesaj : "Harika! Backend ile iletişim kurdun.",
+    durum : "Başarılı",
+    tarih : new Date()
+  });
+});
+
+// Sunucuyu başlatma
+app.listen(PORT, () => {
+    console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor...`);
+    console.log('Test etmek için tarayıcıda açabilirsin');
+});
